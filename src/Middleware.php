efabcbf4fbbeec17118d4e6710a7a9fc51f17631
Jarret Byrne <jbyrne@JarretB-MBR.local>
2015-07-27 11:28:42 -0400

    Initial commit of middleware and composer file.

---
+++ src/Middleware.php
@@ -0,0 +1,58 @@
+<?php
+namespace Stratedge\Quint\Middleware;
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+abstract class Middleware {
+
+    protected $request;
+    protected $response;
+
+    final public function __invoke(RequestInterface $request, ResponseInterface $response, callable $next)
+    {
+        $this->request = $request;
+        $this->response = $response;
+
+        $proceed = $this->before();
+
+        if ($proceed !== false) {
+            $this->response = $next($this->request, $this->response);
+
+            $this->after();
+        }
+
+        return $this->response;
+    }
+
+    public function before()
+    {
+        return true;
+    }
+
+    public function after()
+    {
+        //No-op
+    }
+
+    public function setRequest(RequestInterface $request)
+    {
+        $this->request = $request;
+    }
+
+    public function getRequest()
+    {
+        return $this->request;
+    }
+
+    public function setResponse(ResponseInterface $response)
+    {
+        $this->response = $response;
+    }
+
+    public function getResponse()
+    {
+        return $this->response;
+    }
+
+}
